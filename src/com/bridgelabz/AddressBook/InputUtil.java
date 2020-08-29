569ae491a37e2d264919ad2b39721941b41b846f
soumen-gif <soumentaki1996@gmail.com>
2020-08-29 17:18:25 +0530

    Added: All the user details are added

---
+++ src/com/bridgelabz/AddressBook/InputUtil.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.AddressBook;
+
+import java.util.Scanner;
+
+    public class InputUtil
+    {
+        private final static Scanner sc = new Scanner(System.in);
+        public static int getIntValue()
+        {
+            return sc.nextInt();
+        }
+        public static String getStringValue()
+        {
+            return sc.next();
+        }
+    }
+
